fb4424f776530d2c2d89f5533295c70e07332a7a
ksumeyye <sumeyyeekaya@outlook.com>
2024-11-17 23:57:40 +0300

    About kısmını oluşturuyorum.

--- MyPortfolio/ViewComponents/_AboutComponentPartial.cs
+++ MyPortfolio/ViewComponents/_AboutComponentPartial.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _AboutComponentPartial:ViewComponent
     {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
         public IViewComponentResult Invoke()
         {
             return View();
