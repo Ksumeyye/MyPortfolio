07fa049f5439b9d9bb278d3b93c2dfa7a1200d6e
ksumeyye <sumeyyeekaya@outlook.com>
2024-11-18 22:50:52 +0300

    Admin-Deneyimlerim kısmındayım.

--- MyPortfolio/ViewComponents/_AboutComponentPartial.cs
+++ MyPortfolio/ViewComponents/_AboutComponentPartial.cs
@@ -8,6 +8,9 @@ namespace MyPortfolio.ViewComponents
         MyPortfolioContext portfolioContext = new MyPortfolioContext();
         public IViewComponentResult Invoke()
         {
+            ViewBag.aboutTitle=portfolioContext.Abouts.Select(x=>x.Title).FirstOrDefault();
+            ViewBag.aboutSubDescription = portfolioContext.Abouts.Select(x => x.SubDescription).FirstOrDefault();
+            ViewBag.aboutDetail = portfolioContext.Abouts.Select(x => x.Details).FirstOrDefault();
             return View();
         }
     }
