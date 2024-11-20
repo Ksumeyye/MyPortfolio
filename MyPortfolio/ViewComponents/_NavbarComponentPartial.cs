e52b2193969ba1b8026cc8c1605fcb120b0b23a7
ksumeyye <sumeyyeekaya@outlook.com>
2024-11-20 23:45:30 +0300

    Navbarda değişiklikler yaptım.

--- MyPortfolio/ViewComponents/_NavbarComponentPartial.cs
+++ MyPortfolio/ViewComponents/_NavbarComponentPartial.cs
@@ -1,16 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
-    public class _NavbarComponentPartial:ViewComponent
-    {
-        MyPortfolioContext context=new MyPortfolioContext();
-        public IViewComponentResult Invoke()
-        {
-            ViewBag.todolistCount = context.ToDoLists.Where(x => x.Status == false).Count(); //Henüz yapılmamış işlemlerin sayısını bana bildirir.
-            var values=context.ToDoLists.Where(x=>x.Status==false).ToList(); //Henüz yapılmamış işlemleri bana listeler.
-            return View(values);
-        }
-    }
+	public class _NavbarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
 }
