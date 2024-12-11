9e4cca0997eb3e04082f67ad95c8d726f4732686
Ksumeyye <sumeeyyekaya@gmail.com>
2024-12-11 23:52:51 +0300

    Mesaj gönderme işlemlerini tamamladım.

--- MyPortfolio/Controllers/MessageController.cs
+++ MyPortfolio/Controllers/MessageController.cs
@@ -1,16 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+using MyPortfolio.Models; // Form Modelini buraya eklemelisiniz.
 
 namespace MyPortfolio.Controllers
 {
     public class MessageController : Controller
     {
+
         MyPortfolioContext context = new MyPortfolioContext();
-        public IActionResult Inbox() //Inbox(gelen mesajlar anlamına gelir)
+
+
+        // Inbox - Gelen mesajlar
+        public IActionResult Inbox()
         {
             var values = context.Messages.ToList();
             return View(values);
         }
+
+        // Message Detail - Mesaj detayını gösterme
+        public IActionResult MessageDetail(int id)
+        {
+            var value = context.Messages.Find(id);
+            return View(value);
+        }
+        // Mesaj gönderme formu
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+        // Mesajı veritabanına kaydetme
+        [HttpPost]
+        public IActionResult SendMessage(Message message)
+        {
+            if (ModelState.IsValid) // Form verilerinin geçerli olup olmadığını kontrol eder.
+            {
+                context.Messages.Add(message); // Veritabanına ekleme
+                context.SaveChanges(); // Değişiklikleri kaydetme
+                TempData["MessageSent"] = "Mesajınız başarıyla gönderildi!"; // Gönderim başarılı mesajı
+                return RedirectToAction("Inbox"); // Gönderildikten sonra gelen kutusuna yönlendirir
+            }
+
+            // Eğer formda hata varsa, mesajı tekrar gösterir.
+            return View(message);
+        }
+        // Okundu durumunu değiştirme
         public IActionResult ChangeIsReadToTrue(int id)
         {
             var value = context.Messages.Find(id);
@@ -18,6 +53,7 @@ namespace MyPortfolio.Controllers
             context.SaveChanges();
             return RedirectToAction("Inbox");
         }
+
         public IActionResult ChangeIsReadToFalse(int id)
         {
             var value = context.Messages.Find(id);
@@ -25,17 +61,14 @@ namespace MyPortfolio.Controllers
             context.SaveChanges();
             return RedirectToAction("Inbox");
         }
+
+        // Mesaj silme
         public IActionResult DeleteMessage(int id)
         {
             var value = context.Messages.Find(id);
             context.Messages.Remove(value);
             context.SaveChanges();
             return RedirectToAction("Inbox");
-        }
-        public IActionResult MessageDetail(int id)
-        {
-            var value=context.Messages.Find(id);
-            return View(value);
         }
     }
 }
---
+++ MyPortfolio/ViewComponents/_MessageComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _MessageComponentPartial : ViewComponent
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            var values = context.Messages.ToList();
+            return View(values);
+        }
+    }
+}
